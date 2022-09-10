bfc70f2782b3ac8973f864c00810f2a24c2dd667
Teju Nareddy <tejunareddy@gmail.com>
2022-09-10 04:23:02 +0000

    Add manifest

---
+++ custom_components/templight_sensor/README.md
@@ -0,0 +1,3 @@
+# Home Assistant Integration: TempLight Sensor
+
+TODO
--- custom_components/templight_sensor/config_flow.py
+++ custom_components/templight_sensor/config_flow.py
@@ -16,9 +16,6 @@ DATA_SCHEMA = vol.Schema({})
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow. Current implementation has no config."""
 
-    VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_ASSUMED
-
     async def async_step_user(self, user_input=None) -> data_entry_flow.FlowResult:
         """Handle the no-config step."""
         return self.async_create_entry(title=ENTRY_TITLE, data=dict({}))
